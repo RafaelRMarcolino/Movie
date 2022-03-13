d6e9b1e054d34951aefa5173fba892a90dcf004b
rafa <ralfrr55@gmail.com>
2022-03-13 14:42:49 -0300

    nav

--- frontend/src/components/index.tsx
+++ frontend/src/components/index.tsx
@@ -1,20 +1,21 @@
 import {ReactComponent as GithubIcon} from '../assets/img/gitsvg.svg'
+import './style.css'
 
 function Navbar(){
     return(
-
     <header>
         <nav>
-            <div>
-
-                <h1>Movie</h1>
-                <div>
-                     <GithubIcon/>
+            <div className='container'>
+                <div className='dsmovie-nav-content'>
+                        <h1>Movie</h1>
                     <a href="https://www.linkedin.com/in/rafael-reinaldo-marcolinoproj/">
-                    /Github
+                        <div className='dsmovie-contact-container'>
+                            <GithubIcon/>
+                            <p className='dsmovie-contact-link'>/Github</p> 
+                        </div>
                     </a>
                 </div>
-            </div>  
+            </div>
         </nav>
     </header>
     )
