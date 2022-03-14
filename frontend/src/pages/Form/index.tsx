a88d3e1d69110935d9546e714b58acb381961307
rafa <ralfrr55@gmail.com>
2022-03-14 05:24:50 -0300

    form

--- frontend/src/pages/Form/index.tsx
+++ frontend/src/pages/Form/index.tsx
@@ -1,6 +1,50 @@
+import "./style.css"
 function Form(){
+
+    const movie = {
+        id: 1,
+        image: "https://br.web.img3.acsta.net/c_310_420/medias/nmedia/18/91/08/82/20128877.JPG",
+        title: "",
+        count: 2,
+        score:4.5
+    }
+
     return(
-        <h1>Pages de Form</h1>
+        <div className="dsmovie-form-container">
+            <img className="dsmovie-movie-card-image" src={movie.image} alt={movie.title} />
+            <div className="dsmovie-card-bottom-container">
+                <h3> {movie.title}</h3>
+                <form className="dsmovie-form">
+                    <div className="form-group dsmovie-form-group">
+                        <label htmlFor="email"> Informe seu email </label>
+                        <input type="email" className="form-control" id="email" />
+
+                    </div>  
+                    <div className="form-group dsmovie-form-group">
+                        <label htmlFor="score">Informe sua avalicao</label>
+                        <select className="form-control" id="score">
+                            <option>1</option>    
+                            <option>2</option>
+                            <option>3</option>
+                            <option>4</option>
+                            <option>5</option>
+                        </select>
+                    </div>
+                    <div className="dsmovie-from-btn-container">
+                       
+                        <button type="submit" className="btn btn-primary dsmovie-btn"> Salver </button> 
+   
+                    </div>
+
+
+                </form>   
+                <button className="btn btn-primary dsmovie-btn dsmovie-btn2  mt-3"> Cancelar </button>    
+
+
+            </div>
+
+        </div>
+
     )
 }
 export default Form
