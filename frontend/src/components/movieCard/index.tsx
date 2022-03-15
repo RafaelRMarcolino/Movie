c36e6435d3ef476ba0d6ad92bc17aa0aa3c8fa75
rafa <ralfrr55@gmail.com>
2022-03-15 06:28:33 -0300

    cardmovie

--- frontend/src/components/movieCard/index.tsx
+++ frontend/src/components/movieCard/index.tsx
@@ -1,7 +1,7 @@
 import MovieScore from "components/movieScore"
 
 function MovieCard(){
-    
+
     const movie = {
         id: 1,
         image: "https://br.web.img3.acsta.net/c_310_420/medias/nmedia/18/91/08/82/20128877.JPG",
@@ -11,14 +11,14 @@ function MovieCard(){
     }
 
     return(
-        <div>
-            <img className="dsmovie-movie-card-image" src={movie.image} alt={movie.title} />
-            <div className="dsmovie-card-buttom-container">
-                <h3>{movie.title}</h3>
-                <MovieScore/>
-                <div className="btn btn-primary dsmovie-btn"> Avaliar </div>
-            </div>
-        </div>
+            <div>
+                    <img className="dsmovie-movie-card-image" src={movie.image} alt={movie.title} />
+                    <div className="dsmovie-card-bottom-container">
+                        <h3>{movie.title}</h3>
+                        <MovieScore/>
+                        <div className="btn btn-primary dsmovie-btn"> Avaliar </div>
+                    </div>
+                </div> 
 
     )
 }
